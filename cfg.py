402c45da31c8d68192ace873e7b740a013e48483
Ajitem Joshi <25638226+ExplosionArt@users.noreply.github.com>
2020-08-30 16:07:50 +0530

    Added Exceptions and Handling of old links

--- cfg.py
+++ cfg.py
@@ -2,7 +2,8 @@ config = {
     "root_url" : "https://flinkhub.com/",
     "db" : 'crawler_db',
     "collection" : 'links',
+    "file_dir" : "response_files\\",
     "max_threads" : 5,
     "sleep_time" : 5,
-    "max_links" : 200
+    "max_links" : 5000
 }
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Crawler script
 import requests
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from os import path, makedirs
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
@@ -8,7 +9,8 @@ from time import sleep
 from cfg import config
 from utils import save_file, mark_link_crawled, get_random_file_name
 
-# First try the simple method, without OOP
+def previous_day():
+    return datetime.now() - timedelta(hours = 24)
 
 def SpiderBot(links_collection):
     # Do this process forever
@@ -17,21 +19,24 @@ def SpiderBot(links_collection):
         links = links_collection.find()
         #print('I have fetched links from the database')
 
-        #num_links = links_collection.count_documents({})
-        #print('Number of links I found: ' + str(num_links))
-
-        # If there are links that are not crawled yet, which are within max_links
-        if links_collection.count_documents({"isCrawled" : False}) == 0:
+        # Check if max limit reached
+        if links_collection.count_documents({}) >= config['max_links']:
             # Print max limit reached
             print('Maximum limit reached')
             # Ignore everything and continue
+            # continue
+            # Database limit is reached. However, all links may not be crawled yet.
+            
+
+        if links_collection.count_documents({"isCrawled" : False}) == 0 and links_collection.count_documents({"lastCrawlDt" : { "$lt": previous_day()}}) == 0:
+            # All links in DB crawled. Ignore everything and continue
+            print('All Links crawled')
             continue
 
-        # For each link
+        # Define a function for ThreadPoolExecutor For each link
         def Execute(link):
             # Check If link is crawled in the last 24 hours
-            previous_day = datetime.now() - timedelta(hours = 24)
-            if link['isCrawled'] and link['lastCrawlDt'] >= previous_day:
+            if link['isCrawled'] and link['lastCrawlDt'] >= previous_day():
                 # Ignore this link and continue
                 #continue
                 return
@@ -45,7 +50,36 @@ def SpiderBot(links_collection):
                 print(url)
                 try:
                     r = requests.get(url)
+
+                except requests.exceptions.HTTPError:
+                    # Invalid HTTP Response
+                    print("HTTP exception for " + url)
+                    mark_link_crawled(links_collection, url, datetime.now(), None, None, None, None)
+
+                except requests.exceptions.SSLError:
+                    # Some problematic link(like telegram app link) has SSL Errors, which should not be encounterd again for today
+                    print("SSL exception for " + url)
+                    return
+
+                except requests.exceptions.Timeout:
+                    print("Timeout exception for " + url)
+                    # Do nothing and simply try to get it again in next cycle
+                    return
+
+                except requests.exceptions.ConnectionError:
+                    print("Connection Error for " + url)
+                    # Connection Error, Do nothing and try again in next cycle
+                    return
+
+                except requests.exceptions.RequestException:
+                    print("Request Exception for " + url)
+                    # Ambiguous Exception
+                    return
+                
                 except Exception:
+                    print("Exception for " + url)
+                    # Any other exception which is not handled, mark the link as crawled and move on
+                    mark_link_crawled(links_collection, url, datetime.now(), None, None, None, None)
                     return
 
                 # Check the status code
@@ -63,14 +97,24 @@ def SpiderBot(links_collection):
                 if 'text/html' in content_type:
                     # Extract <a href = ""> links
                     parsed_html = BeautifulSoup(r.content, "html.parser")
-                    file_path = "response_files\\" + get_random_file_name(content_type)
+
+                    # Check if the current url was already crawled and 24 hours have passed
+                    if link['isCrawled'] and link['lastCrawlDt'] < previous_day():
+                        # Get the file path from database entry, and overwrite the same file
+                        file_path = link['filePath']
+                    else:
+                        # This file was not crawled previously, so get a new name
+                        file_path = config['file_dir'] + get_random_file_name(content_type)
 
                     # Save all valid links to database
                     for a in parsed_html.find_all('a', href = True):
-                        # First check if database size is already at max_links or not
+
+                        # First check if database size is already at max_links
                         if links_collection.count_documents({}) >= config['max_links']:
+                            # Do not add any new links to database, simply break
                             break
 
+                        # Check for relative links
                         if a['href'] == "" or a['href'][0] == '//':
                             continue
 
@@ -80,20 +124,20 @@ def SpiderBot(links_collection):
                         if not (requests.compat.urlparse(a['href']).scheme and requests.compat.urlparse(a['href']).netloc):
                             continue
 
-                        # Add entry to database only if link is unique
+                        # Add new entry to database only if link is unique
                         if links_collection.count_documents({"link" : a['href']}) == 0:
                             links_collection.insert_one({
                                 "link" : a['href'], 
                                 "sourceLink" : url, 
                                 "isCrawled" : False, 
-                                "lastCrawlDt" : datetime(2020, 1, 1, 0, 0), 
+                                "lastCrawlDt" : None, 
                                 "responseStatus" : None, 
                                 "contentType" : None, 
                                 "contentLength" : None, 
                                 "filePath" : None, 
                                 "createdAt" : datetime.now()
                             })
-                
+                    
                     # Save the file on disk
                     save_file(r.content, file_path)
 
@@ -103,7 +147,7 @@ def SpiderBot(links_collection):
                 # If content type is not html
                 else:
                     # Based on content type, create a file name
-                    file_path = "response_files\\" + get_random_file_name(content_type)
+                    file_path = config['file_dir'] + get_random_file_name(content_type)
 
                     # Save the file on disk
                     save_file(r.content, file_path)
@@ -111,9 +155,13 @@ def SpiderBot(links_collection):
                     # Mark link as isCrawled = True and continue with next link
                     mark_link_crawled(links_collection, url, datetime.now(), r.status_code, content_type, len(r.content), file_path)
         
+        # Multithreading with 'max_threads'
         with ThreadPoolExecutor(max_workers = config['max_threads']) as executor:
             executor.map(Execute, links)
+        
+        # Print that one cycle is complete
         print('One Cycle Complete. Sleep for 5 seconds')
+
         # Sleep for 5 seconds
         sleep(config['sleep_time'])
 
@@ -127,13 +175,17 @@ if __name__ == "__main__":
     except Exception as inst:
         print("MongoDB connection Error", inst.args)
 
+    # Check if the response-files directory is already present or not, else create it
+    if not path.exists(config['file_dir']):
+        makedirs(config['file_dir'])
+
     # Check if root URL exists in database
     if links_collection.find_one({"link" : config['root_url']}) is None:
         links_collection.insert_one({
             "link" : config['root_url'],
             "sourceLink" : "",
             "isCrawled" : False,
-            "lastCrawlDt" : datetime(2020, 1, 1, 0, 0),
+            "lastCrawlDt" : None,
             "responseStatus" : 0,
             "contentType" : "",
             "contentLength" : 0,
@@ -141,4 +193,5 @@ if __name__ == "__main__":
             "createdAt" : datetime.now()
         })
     
+    # Call the SpiderBot
     SpiderBot(links_collection)
